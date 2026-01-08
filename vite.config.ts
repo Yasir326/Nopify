8700ae2617f4a3088aa0f2a62c262525efe26f10
yasir-dg <yasirk2190@gmail.com>
2026-01-08 11:23:23 +0000

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
+  base: "/Nopify/",
   server: {
     host: "::",
     port: 8080,
